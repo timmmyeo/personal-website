ec2ce672c5395d5201993eeae7528494b25036b0
Timothy Yeo <timothyyeo121@gmail.com>
2020-09-19 16:29:34 +0100

    Fixed links, updated icons

--- src/components/IconLinks.js
+++ src/components/IconLinks.js
@@ -5,27 +5,28 @@ import DescriptionIcon from '@material-ui/icons/Description';
 import IconButton from '@material-ui/core/IconButton';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import CV from '../files/CV.pdf';
 
 
 export default function IconLinks() {
   return (
     <>
-      <Link href="https://google.com">
+      <Link href={CV}>
         <IconButton color="primary" aria-label="CV">
           <DescriptionIcon />
         </IconButton>
       </ Link>
-        <Link href="https://google.com">
+        <Link href="https://github.com/timmmyeo">
           <IconButton color="primary" aria-label="GitHub">
             <GitHubIcon />
           </IconButton>
         </ Link>
-        <Link href="https://google.com">  
+        <Link href="https://www.linkedin.com/in/timothy-yeo-399326117/">  
           <IconButton color="primary" aria-label="LinkedIn">
               <LinkedInIcon />
           </IconButton>
         </ Link>
-        <Link href="https://google.com">  
+        <Link href="mailto:timothyyeo121@gmail.com">  
           <IconButton color="primary" aria-label="Email">
               <EmailIcon />
           </IconButton>
